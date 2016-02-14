df8a358f32915f19a671585770eb0924b96efa13
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2016-02-13 19:48:52 -0800

    Updates the app and controllers so that the index page will load

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -18,6 +17,15 @@ mongoose.connect('mongodb://localhost/babble');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// controller setup
+fs.readdirSync('./controllers').forEach(function (file) {
+  if(file.substr(-3) == '.js') {
+      route = require('./controllers/' + file);
+      route.controller(app);
+  }
+});
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -26,13 +34,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// dynamically include routes (Controller)
-fs.readdirSync('./controllers').forEach(function (file) {
-  if(file.substr(-3) == '.js') {
-      route = require('./controllers/' + file);
-      route.controller(app);
-  }
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
