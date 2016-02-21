6ee36b76f86f18e682ac9f90200ea74f5828f5b6
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2016-02-21 14:46:39 -0800

    Adds content model

---
+++ app/models/content.js
@@ -0,0 +1,18 @@
+var mongoose  = require('mongoose');
+var Schema    = mongoose.Schema;
+var markdown  = require('markdown').markdown;
+
+var Content = new Schema({
+  type: String,
+  content: String
+});
+
+Content.methods.render = function(){
+  if(this.type == 'markdown'){
+    return markdown.toHTML(this.content);
+  } else {
+    return this.content;
+  }
+}
+
+module.exports = mongoose.model('Content', Content);
