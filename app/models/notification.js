4a15ec6643dc4add07ab1d1c9bdb90e5d19a0f6f
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2016-02-21 14:47:33 -0800

    Adds the status member to the schema for notification

--- app/models/notification.js
+++ app/models/notification.js
@@ -3,7 +3,8 @@ var Schema    = mongoose.Schema;
 
 var Notification = new Schema({
   type: String,
-  content: { type: Schema.Types.ObjectId, ref: 'Content' }
+  content: { type: Schema.Types.ObjectId, ref: 'Content' },
+  status: String
 });
 
 module.exports = mongoose.model('Notification', Notification);
