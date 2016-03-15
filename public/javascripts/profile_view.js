a8e36d9254c3268a4ca4debbf44d71a2dcc9a91f
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2016-03-15 13:04:24 -0600

    Adding group interactions to profile_view

--- public/javascripts/profile_view.js
+++ public/javascripts/profile_view.js
@@ -4,7 +4,37 @@ angular.module('babble.profile_view', []).controller('ProfileViewControl', [
 
 
     $scope.init = function(){
-      console.log('Working!');
+      $scope.profile = window.location.href.split('/')[4];
+    }
+
+    $scope.create_group_trigger = function(){
+      $("#group_create_modal").foundation("reveal", "open");
+    }
+
+    $scope.select_friends = function(){
+      $http({
+        url: '/profile/' + $scope.profile + '/friends_list',
+        method: 'GET'
+      }).then(function(res){
+        $scope.friend_list = res.data;
+        console.log(res);
+      }).catch(function(res){
+        console.log(res);
+      });
+    }
+
+    $scope.get_groups = function(){
+      $scope.selected_tab = 'groups';
+      console.log('Retrieving group list');
+      $http({
+        url: '/profile/get_group_list/' + $scope.profile,
+        method: 'GET'
+      }).then(function(res){
+        $scope.group_list = res.data;
+        console.log(res);
+      }).catch(function(res){
+        console.log(res);
+      });
     }
 
   }
